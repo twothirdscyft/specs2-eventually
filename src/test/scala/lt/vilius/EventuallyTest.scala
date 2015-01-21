d3b63717e15c5836c1ffb6caae0780819929949e
Vilius Lukosius <vilius@wix.com>
2015-01-21 09:58:35 +0200

    Added more variations

--- src/test/scala/lt/vilius/EventuallyTest.scala
+++ src/test/scala/lt/vilius/EventuallyTest.scala
@@ -13,18 +13,20 @@ import scala.concurrent.duration.Duration
 class EventuallyTest extends SpecificationWithJUnit {
 
   "eventually" should {
-    "work properly without custom timeout" in {
-      eventually {
-        1 mustEqual 1
-      }
+    "work properly" in {
+      eventually { 1 mustEqual 1 }
+      1 must eventually(===(1))
+      1 must eventually(5, Duration(1, TimeUnit.SECONDS))(===(1))
+      1 must beEqualTo(1).eventually
+      1 must beEqualTo(1).eventually(5, Duration(1, TimeUnit.SECONDS))
     }
 
     /**
-      * Error:(24, 11) type mismatch;
-      * found   : org.specs2.matcher.MatchResult[Any]
-      * required: org.specs2.matcher.Matcher[?]
-      *         1 mustEqual 1
-      */
+     * Error:(24, 11) type mismatch;
+     * found   : org.specs2.matcher.MatchResult[Any]
+     * required: org.specs2.matcher.Matcher[?]
+     * 1 mustEqual 1
+     */
     "work properly with custom timeout" in {
       eventually(5, Duration(1, TimeUnit.SECONDS)) {
         1 mustEqual 1
@@ -32,5 +34,4 @@ class EventuallyTest extends SpecificationWithJUnit {
     }
 
   }
-
 }
